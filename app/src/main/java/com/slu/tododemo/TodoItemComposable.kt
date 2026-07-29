41e09ea1cfa1e5e56190a788cbdf27a6de99b94a
cameronmcnz <tanishqsatsangi@gmail.com>
2026-07-29 11:09:08 +0530

    Initial commit for standalone TodoDemo project.
    Split TodoDemo out from the Workspace monorepo into its own independent Git repository.

---
+++ app/src/main/java/com/slu/tododemo/TodoItemComposable.kt
@@ -0,0 +1,63 @@
+package com.slu.tododemo
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+    @Composable
+    fun TodoItem() {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text("Title")
+            Text("Subtitle")
+
+            Row() {
+                Text("Priority")
+               // Spacer(Modifier.weight(1f))
+                Button(
+                    contentPadding = PaddingValues(0.dp),
+                    modifier = Modifier.size(48.dp),
+                    onClick = {
+                        // some action
+                    }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.outline_box_edit_24),
+                        contentDescription = "Edit",
+                        modifier = Modifier
+                    )
+
+                }
+                Button(
+                    contentPadding = PaddingValues(0.dp),
+                    modifier = Modifier.size(48.dp),
+                    onClick =
+                        {
+                            //some action
+                        }
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_done),
+                        contentDescription = "Edit",
+                        modifier = Modifier
+                    )
+                }
+            }
+
+        }
+    }
+
+    @Preview(showSystemUi = true, showBackground = true)
+    @Composable
+    fun TodoItemPreview() {
+        TodoItem()
+    }
